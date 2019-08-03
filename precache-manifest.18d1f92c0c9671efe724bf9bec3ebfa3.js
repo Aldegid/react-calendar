f85f501cce932448580ca26967262801f85da86c
Aldegid <aldegid@gmail.com>
2019-08-03 15:26:08 +0300

    Updates

---
+++ precache-manifest.18d1f92c0c9671efe724bf9bec3ebfa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52dd25770ef8a3e844083d9c432a9f1",
+    "url": "/react-calendar/index.html"
+  },
+  {
+    "revision": "aebc0c241c88ce75a1d3",
+    "url": "/react-calendar/static/css/2.8283786d.chunk.css"
+  },
+  {
+    "revision": "ff57f4dd8050bb1a4f72",
+    "url": "/react-calendar/static/css/main.12ab31f0.chunk.css"
+  },
+  {
+    "revision": "aebc0c241c88ce75a1d3",
+    "url": "/react-calendar/static/js/2.4d0a4886.chunk.js"
+  },
+  {
+    "revision": "ff57f4dd8050bb1a4f72",
+    "url": "/react-calendar/static/js/main.08891015.chunk.js"
+  },
+  {
+    "revision": "26d1c32bdeb999eefa04",
+    "url": "/react-calendar/static/js/runtime~main.40a83ec6.js"
+  }
+]);
